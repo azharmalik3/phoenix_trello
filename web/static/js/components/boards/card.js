c57661eb7696869a0cac4f119ec083c5ee5c1ad6
azharmalik3 <azharmalik02@gmail.com>
2016-04-05 13:54:02 +0500

    added card js and remove idea files

---
+++ web/static/js/components/boards/card.js
@@ -0,0 +1,25 @@
+/**
+ * Created by azhar on 05/04/16.
+ */
+
+import React, {PropTypes} from 'react';
+import { push }           from 'react-router-redux';
+
+export default class BoardCard extends React.Component {
+  _handleClick() {
+    this.props.dispatch(push(`/boards/${this.props.id}`));
+  }
+
+  render() {
+    return (
+      <div id={this.props.id} className="board" onClick={::this._handleClick}>
+        <div className="inner">
+          <h4>{this.props.name}</h4>
+        </div>
+      </div>
+    );
+  }
+}
+
+BoardCard.propTypes = {
+};
